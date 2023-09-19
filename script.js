8e0bf144ac62878af495a64bff317a2e36f32938
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2023-09-19 10:32:40 +0300

    Minor addition to the checkPasswords() function
    Now it can also disable and enable the validity of
    of the passoword elements when one of them is re-emptied
    after value has been added to them

--- script.js
+++ script.js
@@ -4,23 +4,9 @@ let errorMessage = document.querySelector(".error-message");
 
 let pass, confirmPass;
 
-function isPassEqual(pass, confirmPass) {
-  pass === confirmPass ? true : false;
-}
-
-password.addEventListener("input", function () {
-  pass = this.value;
-  console.log(this.value);
-  console.log(document.activeElement);
-  checkPasswords();
-});
-
-confirmPassword.addEventListener("input", function () {
-  confirmPass = this.value;
-  checkPasswords();
-});
-
-document.addEventListener("click", checkPasswords);
+// function isPassEqual(pass, confirmPass) {
+//   pass === confirmPass ? true : false;
+// }
 
 function checkPasswords() {
   if (pass && confirmPass) {
@@ -50,5 +36,28 @@ function checkPasswords() {
       }
       console.log("error is hidden");
     }
+  } else {
+    if (errorMessage.classList.contains("show-error")) {
+      errorMessage.classList.remove("show-error");
+    }
+    if (!password.checkValidity() || !confirmPassword.checkValidity()) {
+      password.setCustomValidity("");
+      confirmPassword.setCustomValidity("");
+    }
+    console.log("error is hidden");
   }
 }
+
+password.addEventListener("input", function () {
+  pass = this.value;
+  console.log(this.value);
+  console.log(document.activeElement);
+  checkPasswords();
+});
+
+confirmPassword.addEventListener("input", function () {
+  confirmPass = this.value;
+  checkPasswords();
+});
+
+document.addEventListener("click", checkPasswords);
